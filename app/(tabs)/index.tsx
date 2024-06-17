d9c8a19742520be482de8736350785a90a5442ac
mañana <ericalaa@ymail.com>
2024-06-17 11:06:07 +0200

    new

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,38 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useState } from 'react';
+import {app} from '../../services/firebase'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
 
 export default function TabOneScreen() {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+      function signUp() {
+        const auth = getAuth(app);
+
+        createUserWithEmailAndPassword(
+            auth,
+            email,
+            pass
+        )
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+            
+    }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <TextInput style={styles.input} value={email} onChangeText={(e)=>setEmail(e)} />
+      <TextInput style={styles.input} secureTextEntry={true} value={pass} onChangeText={(e)=>setPass(e)} />
+
+      <TouchableOpacity style={styles.touchable} onPress={signUp}>
+      <Text>Hola</Text>
+      </TouchableOpacity>
+
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
@@ -28,4 +54,17 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  input: {
+    height: 40,
+    width: "70%",
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "white"
+  },
+  touchable:{
+    padding:10,
+    color: 'white',
+    backgroundColor: 'blue',
+  },
+
 });
